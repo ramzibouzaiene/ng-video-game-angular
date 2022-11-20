ed21602b7370597431b921828101f0e0938ecf18
ramzibouzaiene <ramzibouzaiene00@gmail.com>
2022-11-20 15:13:40 +0100

    add services and models to the project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  BASE_URL: 'https://rawg-video-game-database.p.rapidapi.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  BASE_URL: 'https://rawg-video-game-database.p.rapidapi.com'
+
 };
 
 /*
