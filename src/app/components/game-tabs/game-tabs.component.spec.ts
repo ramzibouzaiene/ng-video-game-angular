b8f7b494ab6fc9316b2a5c1144aebc113a6938a4
ramzibouzaiene <ramzibouzaiene00@gmail.com>
2022-11-22 21:29:37 +0100

    adding tables for games details adding the style

---
+++ src/app/components/game-tabs/game-tabs.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GameTabsComponent } from './game-tabs.component';
+
+describe('GameTabsComponent', () => {
+  let component: GameTabsComponent;
+  let fixture: ComponentFixture<GameTabsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ GameTabsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GameTabsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
